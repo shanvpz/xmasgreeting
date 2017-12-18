6002db67fc5947a16e94d9611d8f71e0c56e10e0
jobinjnj <9847116011.zion>
2017-12-18 12:23:12 +0530

    Splash Screen

--- app/src/main/java/in/techfantasy/xmasgreeting/Splash.java
+++ app/src/main/java/in/techfantasy/xmasgreeting/Splash.java
@@ -1,13 +1,37 @@
 package in.techfantasy.xmasgreeting;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+        Thread t=new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                    Intent in=new Intent(Splash.this,MainActivity.class);
+                    startActivity(in);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        getSupportActionBar().hide();
+        t.start();
+
     }
+
 }
