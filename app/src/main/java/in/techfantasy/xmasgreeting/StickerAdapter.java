4825abed07de7e55a050927bee97dd0ae87c82a5
shankm <shankm2010@gmail.com>
2017-12-19 16:34:40 +0530

    DataStore class Added

--- app/src/main/java/in/techfantasy/xmasgreeting/StickerAdapter.java
+++ app/src/main/java/in/techfantasy/xmasgreeting/StickerAdapter.java
@@ -18,8 +18,9 @@ import in.techfantasy.xmasgreeting.R;
 
 public class StickerAdapter extends BaseAdapter {
     private Context Context;
-    public StickerAdapter(Context c) {
-
+    int[] images;
+    public StickerAdapter(Context c,int[] images) {
+        this.images=images;
         Context = c;
     }
     @Override
@@ -46,19 +47,5 @@ public class StickerAdapter extends BaseAdapter {
         imageView.setImageResource(images[i]);
         return view1;
     }
-    public int[] images={R.drawable.sticker1,R.drawable.sticker2,R.drawable.sticker3,
-                         R.drawable.stcker4,R.drawable.sticker5,R.drawable.sticker6,
-                         R.drawable.sticker7,R.drawable.sticker9,R.drawable.sticker8,
-                         R.drawable.sticker10,R.drawable.sticker11,
-                         R.drawable.sticker13,R.drawable.sticker14, R.drawable.sticker15,
-                         R.drawable.sticker16,R.drawable.sticker17,R.drawable.sticker18,
-                         R.drawable.sticker19,R.drawable.sticker20,R.drawable.sticker21,
-                         R.drawable.sticker22,R.drawable.sticker23,R.drawable.sticker24,
-                         R.drawable.sticker25,R.drawable.sticker26,R.drawable.sticker27,
-                         R.drawable.sticker28,R.drawable.sticker29,R.drawable.sticker30,
-                         R.drawable.sticker31,R.drawable.sticker33,R.drawable.sticker34,
-                         R.drawable.sticker36,R.drawable.sticker37,R.drawable.sticker38,
-                         R.drawable.sticker39,
 
-                         };
 }
