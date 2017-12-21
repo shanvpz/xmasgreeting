c251e19f0427ed6e01836ce23b681108aee2becb
shankm <shankm2010@gmail.com>
2017-12-21 11:55:09 +0530

    SnowFall Added, Splash Edited

---
+++ app/src/main/java/in/techfantasy/xmasgreeting/Snow/Random.java
@@ -0,0 +1,24 @@
+package in.techfantasy.xmasgreeting.Snow;
+
+/**
+ * Created by campusiq on 21/12/17.
+ */
+
+class Random {
+    private static final java.util.Random RANDOM = new java.util.Random();
+
+    public float getRandom(float lower, float upper) {
+        float min = Math.min(lower, upper);
+        float max = Math.max(lower, upper);
+        return getRandom(max - min) + min;
+    }
+
+    public float getRandom(float upper) {
+        return RANDOM.nextFloat() * upper;
+    }
+
+    public int getRandom(int upper) {
+        return RANDOM.nextInt(upper);
+    }
+
+}
